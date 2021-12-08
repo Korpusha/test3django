3f5b8b06cb0aecb6ef0d32e801afc58811499eff
Korpusha <daniilzzz45@gmail.com>
2021-12-08 17:56:36 +0200

    module_1_django_fix2

--- mysite/myapp/include_urls/return_urls.py
+++ mysite/myapp/include_urls/return_urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from myapp.views import ReturnProduct, ReturnPage, DeleteReturnObj
 
 urlpatterns = [
     path('', ReturnProduct.as_view(), name='return_'),
     path('page', ReturnPage.as_view(), name='return_page'),
-    path(r'^success-del/(?P<pk>\d+)/$', DeleteReturnObj.as_view(), name='return_del'),
+    re_path(r'^success-del/(?P<pk>\d+)/$', DeleteReturnObj.as_view(), name='return_del'),
 ]
